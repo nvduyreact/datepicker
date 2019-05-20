9aaaebef14146e952e82420c340250ece4b2a3d5
DuyNV <nvduy.reactjs@gmail.com>
2019-05-20 23:30:59 +0700

    pagination

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatPaginatorModule} from '@angular/material';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatAutocompleteModule, MatInputModule, MatNativeDateModule} from '@angular/material';
+// import {MatAutocompleteModule, MatInputModule, MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatAutocompleteModule, MatInputModule, MatNativeDateModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatAutocompleteModule, MatInputModule, MatNativeDateModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatPaginatorModule ],
+  exports: [MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatPaginatorModule],
 })
 export class MaterialModule {}
